ff892223e71ba54e0bf138d26fc4b08a429a5833
Jasio <mcyanek@gmail.com>
2020-09-15 12:47:53 +0200

    adds war plugin into pom

--- src/main/java/com/codecool/hsdecktracker/Main.java
+++ src/main/java/com/codecool/hsdecktracker/Main.java
@@ -6,6 +6,5 @@ public class Main {
 
     public static void main(String[] args) {
         //DBPopulator dbPopulator = new DBPopulator(); //Uncomment to drop DB and populate it with default dataset
-
     }
 }
