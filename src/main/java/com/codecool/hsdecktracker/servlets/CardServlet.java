7ddb8ac1a0ddf30daf72a6a9967a833b78149bc6
Jasio <mcyanek@gmail.com>
2020-09-16 10:36:35 +0200

    removes test code

--- src/main/java/com/codecool/hsdecktracker/servlets/CardServlet.java
+++ src/main/java/com/codecool/hsdecktracker/servlets/CardServlet.java
@@ -7,36 +7,14 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="CardServlet", urlPatterns = {"/cards"}, loadOnStartup = 2)
+@WebServlet(name="CardServlet", urlPatterns = {"/api/v1/cards"}, loadOnStartup = 2)
 
 public class CardServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
-
-        StringBuffer buffer = new StringBuffer();
-        for (int i = 0; i < 10; i++) {
-            buffer.append("<div>");
-            buffer.append("<a href=\"/another?link_id=" + i + "\">");
-            buffer.append("Hello " + i + ". link:");
-            buffer.append("</a>");
-            buffer.append("</div>");
-        }
-
         String linkId = request.getParameter("link_id");
-
-        out.println(
-                "<html>\n" +
-                        "<head><title>Another page</title></head>\n" +
-                        "<body>\n" +
-                        "<h1>Hello CodeCooler!</h1>" +
-                        (linkId == null ?
-                                "<h3>No link was pressed</h3>" :
-                                "<h3>Link " + linkId + " was pressed!</h3>") +
-                        "<br/>" +
-                        "<div>" + buffer.toString() + "</div>" +
-                        "</body></html>"
-        );
+        out.println();
     }
 }
