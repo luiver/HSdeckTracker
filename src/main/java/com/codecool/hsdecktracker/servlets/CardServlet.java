e256f9842a928dd77ea289df30951d829b8b8a06
Jasio <mcyanek@gmail.com>
2020-09-17 13:22:51 +0200

    adds filtering by parameter provided in uri

--- src/main/java/com/codecool/hsdecktracker/servlets/CardServlet.java
+++ src/main/java/com/codecool/hsdecktracker/servlets/CardServlet.java
@@ -18,8 +18,8 @@ public class CardServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
-        String linkId = request.getParameter("link_id");
-        List<Card> cards = CardMenager.getCardMenagerInstance().getCardByClass("DRUID");
+        String cardClass = request.getParameter("class");
+        List<Card> cards = CardMenager.getCardMenagerInstance().getCardByClass(cardClass);
         cards.forEach(c -> out.println(c.toString()));
         out.println();
     }
