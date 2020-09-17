84c24bcb970bdbc108d28bce6fa1b12be12d4ecb
Dominik <domek99@gmail.com>
2020-09-17 23:13:02 +0200

    add try/catch statement to update method

--- src/main/java/com/codecool/hsdecktracker/DAO/UserDao.java
+++ src/main/java/com/codecool/hsdecktracker/DAO/UserDao.java
@@ -60,7 +60,7 @@ public class UserDao extends PostgresDAO<User> implements DAO<User> {
     }
 
     @Override
-    public boolean update(User user) throws ElementNotFoundException, SQLException {
+    public boolean update(User user) throws ElementNotFoundException{
         Connection connection = this.getConnection();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement("UPDATE users SET " +
@@ -74,7 +74,11 @@ public class UserDao extends PostgresDAO<User> implements DAO<User> {
             connection.close();
             return true;
         } catch (SQLException e) {
-            connection.close();
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
             e.printStackTrace();
         }
         return false;
