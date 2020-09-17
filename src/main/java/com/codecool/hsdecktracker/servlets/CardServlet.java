075992906dbfea7b2873447172ace61413676e9e
Jasio <mcyanek@gmail.com>
2020-09-17 18:26:05 +0200

    implements sending response in json format

--- src/main/java/com/codecool/hsdecktracker/servlets/CardServlet.java
+++ src/main/java/com/codecool/hsdecktracker/servlets/CardServlet.java
@@ -2,6 +2,7 @@ package com.codecool.hsdecktracker.servlets;
 
 import com.codecool.hsdecktracker.entitymenager.CardMenager;
 import com.codecool.hsdecktracker.model.Card;
+import org.json.simple.JSONArray;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,15 +12,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import static org.json.JSONArray.*;
+
 @WebServlet(name="CardServlet", urlPatterns = {"/api/v1/cards"}, loadOnStartup = 2)
 public class CardServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
         String cardClass = request.getParameter("class");
         List<String> cards = CardMenager.getCardMenagerInstance().getCardByClass(cardClass);
-        cards.forEach(c -> out.println(c));
-        out.println();
+        String output = "{\"cards\":" + cards + "}";
+        out.println(output);
     }
 }
